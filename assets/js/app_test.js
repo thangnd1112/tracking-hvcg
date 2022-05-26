855aa5f7456cc22511dce1dfda45b42a5a438d16
thangnd1112 <ndthang1992@gmail.com>
2022-05-26 12:11:36 +0700

    test18

--- assets/js/app_test.js
+++ assets/js/app_test.js
@@ -34,7 +34,7 @@ function getApi(url) {
       key[1] +
       "/values/Trang%20tính1?alt=json&access_token=" +
       gettedToken[0].theKeyOfAccessToken.access_token;
-  }, 1000);
+  }, 200);
   
 
   // var url_api = "https://sheets.googleapis.com/v4/spreadsheets/" + key[1] + "/values/Sheet1?alt=json&access_token=" + gettedToken[0].Zb.access_token;
